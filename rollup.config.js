0a7bbf3ad7e5d1fe89301b3b72484f6c5b9b943b
Joseph Dalrymple <swivelgames@gmail.com>
2023-03-07 12:08:24 -0600

    added cjs export

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,16 @@
 import babel from '@rollup/plugin-babel';
+import terser from '@rollup/plugin-terser';
 
 const config = {
-  input: 'src/index.js',
-  output: {
-    dir: 'lib',
-    format: 'esm'
-  },
-  plugins: [babel({ babelHelpers: 'bundled' })]
+	input: 'src/index.js',
+	output: [
+		{ format: 'esm', file: 'lib/index.mjs', },
+		{ format: 'cjs', file: 'lib/index.cjs', },
+	],
+	plugins: [
+		babel({ babelHelpers: 'bundled' }),
+		terser(),
+	],
 };
 
 export default config;
